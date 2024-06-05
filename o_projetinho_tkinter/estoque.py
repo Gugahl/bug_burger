42b0ab6f3e298216b085ac0e7f27c3f18022de11
Gustavo Henrique <ustavo.santos@gmail.com>
2024-06-05 16:32:10 -0300

    Update o_prjetinho

--- o_projetinho_tkinter/estoque.py
+++ o_projetinho_tkinter/estoque.py
@@ -1,103 +1,72 @@
-import os
-import tkinter as tk
-from tkinter import messagebox
+from utils import ler_estoque, escrever_estoque
 
-def ler_estoque(nome_arquivo):
-    estoque = []
-    if os.path.exists(nome_arquivo):
-        with open(nome_arquivo, 'r') as arquivo:
-            for linha in arquivo:
-                nome, qtd = linha.strip().split(',')
-                estoque.append({"nome": nome, "qtd": int(qtd)})
-    return estoque
-
-def escrever_estoque(nome_arquivo, estoque):
-    with open(nome_arquivo, 'w') as arquivo:
-        for item in estoque:
-            arquivo.write(f"{item['nome']},{item['qtd']}\n")
-
-def acessar_estoque_gui():
-    root = tk.Tk()
-    root.title("Estoque")
-    root.geometry("1280x720")
-
-    estoque = ler_estoque("estoque.txt")
-
-    def adicionar_produto():
-        produto = entry_produto.get().upper()
-        qtd = entry_qtd.get()
-
-        if not produto or not qtd:
-            messagebox.showwarning("Campos Vazios", "Por favor, preencha todos os campos!")
-            return
-
-        qtd = int(qtd)
-
-        existe = False
-        for item in estoque:
-            if item["nome"] == produto:
-                item["qtd"] += qtd
-                existe = True
+# Função para acessar o estoque
+def acessar_estoque():
+    nome_arquivo = "estoque.txt"
+    estoque = ler_estoque(nome_arquivo)
+    while True:
+        print("""\nO que você deseja fazer:
+1) Adicionar produto
+2) Remover produto
+3) Ver estoque
+4) Voltar ao menu principal""")
+        escolha = input()
+        match escolha:
+            case "1":
+                produto = {"nome" : None, "qtd" : None}
+                produto["nome"] = input("Insira o nome do produto a ser adicionado: ").upper()
+                while True:
+                    produto["qtd"] = input("Insira a quantidade a ser adicionada ao estoque: ")
+                    if not produto["qtd"].isnumeric():
+                        print("POR FAVOR INSIRA SOMENTE NÚMEROS INTEIROS E POSITIVOS!!!!")
+                    else:
+                        produto["qtd"] = int(produto["qtd"])
+                        break
+                if len(estoque) == 0:
+                    estoque.append(produto)   
+                else:
+                    existe = False
+                    for i in estoque:
+                        if i["nome"] == produto["nome"]:
+                            existe = True
+                            i["qtd"] += produto["qtd"]
+                            break
+                    if not existe:
+                        estoque.append(produto)
+                print(f"{produto['qtd']} UNIDADES DE '{produto['nome']}' FORAM ADICIONADAS AO ESTOQUE")
+                escrever_estoque(nome_arquivo, estoque)
+                input("Aperte enter para voltar...")
+            case "2":
+                if len(estoque) == 0:
+                    print("NÃO HÁ NENHUM PRODUTO NO SEU ESTOQUE!")
+                    input("Aperte enter para voltar...")
+                else:
+                    nome = input("Digite o nome do produto a ser removido: ")
+                    nome = nome.upper()
+                    for i in estoque:
+                        if i.get('nome') == nome:
+                            estoque.remove(i)
+                            print("PRODUTO REMOVIDO COM SUCESSO!")
+                            escrever_estoque(nome_arquivo, estoque)
+                            input("Aperte enter para voltar...")
+                            break
+                    else:
+                        print(f"ERRO! NÃO HÁ PRODUTO CHAMADO '{nome}' NO ESTOQUE")
+                        input("Aperte enter para voltar...")
+            case "3":
+                if len(estoque) == 0:
+                    print("ESTOQUE VAZIO...")
+                    input("Aperte enter para voltar...")
+                else:
+                    print("PRODUTO\t\tESTOQUE")
+                    for i in estoque:
+                        print(f"{i['nome']}\t\t{i['qtd']}")
+                    input("Aperte enter para voltar...")
+            case "4":
                 break
+            case other:
+                print("\033[31mCOMANDO INVÁLIDO!\033[m")
 
-        if not existe:
-            estoque.append({"nome": produto, "qtd": qtd})
-
-        escrever_estoque("estoque.txt", estoque)
-        messagebox.showinfo("Produto Adicionado", f"{qtd} unidades de {produto} foram adicionadas ao estoque!")
-        entry_produto.delete(0, tk.END)
-        entry_qtd.delete(0, tk.END)
-
-    def remover_produto():
-        produto = entry_produto.get().upper()
-
-        if not produto:
-            messagebox.showwarning("Campo Vazio", "Por favor, insira o nome do produto a ser removido!")
-            return
-
-        for item in estoque:
-            if item["nome"] == produto:
-                estoque.remove(item)
-                escrever_estoque("estoque.txt", estoque)
-                messagebox.showinfo("Produto Removido", f"O produto {produto} foi removido do estoque!")
-                entry_produto.delete(0, tk.END)
-                return
-
-        messagebox.showwarning("Produto Não Encontrado", f"O produto {produto} não foi encontrado no estoque!")
-
-    def ver_estoque():
-        if not estoque:
-            messagebox.showinfo("Estoque Vazio", "O estoque está vazio!")
-            return
-
-        mensagem = "Estoque:\n"
-        for item in estoque:
-            mensagem += f"{item['nome']}: {item['qtd']} unidades\n"
-        messagebox.showinfo("Estoque", mensagem)
-
-    frame = tk.Frame(root)
-    frame.pack(pady=20)
-
-    label_produto = tk.Label(frame, text="Produto:")
-    label_produto.grid(row=0, column=0, padx=10, pady=5)
-    entry_produto = tk.Entry(frame)
-    entry_produto.grid(row=0, column=1, padx=10, pady=5)
-
-    label_qtd = tk.Label(frame, text="Quantidade:")
-    label_qtd.grid(row=1, column=0, padx=10, pady=5)
-    entry_qtd = tk.Entry(frame)
-    entry_qtd.grid(row=1, column=1, padx=10, pady=5)
-
-    btn_adicionar = tk.Button(frame, text="Adicionar Produto", width=20, command=adicionar_produto)
-    btn_adicionar.grid(row=2, columnspan=2, padx=10, pady=10)
-
-    btn_remover = tk.Button(frame, text="Remover Produto", width=20, command=remover_produto)
-    btn_remover.grid(row=3, columnspan=2, padx=10, pady=10)
-
-    btn_ver = tk.Button(frame, text="Ver Estoque", width=20, command=ver_estoque)
-    btn_ver.grid(row=4, columnspan=2, padx=10, pady=10)
-
-    root.mainloop()
-
+# Exemplo de chamada à função principal
 if __name__ == "__main__":
-    acessar_estoque_gui()
+    acessar_estoque()
--- o_projetinho_tkinter/main.py
+++ o_projetinho_tkinter/main.py
@@ -1,32 +1,246 @@
 import tkinter as tk
 from tkinter import messagebox
-from estoque import *
-from caixa import *
+from caixa import registrar_venda, calcular_total_vendido, obter_historico_vendas
+from estoque import adicionar_produto, remover_produto, obter_estoque
 
-def main_gui():
-    root = tk.Tk()
-    root.title("Sistema de Gestão")
-    root.geometry("1080x720")
+estoque_insumos = []
 
-    def abrir_caixa():
-        acessar_caixa_gui()
+def adicionar_insumo(nome, quantidade):
+    global estoque_insumos
+    for item in estoque_insumos:
+        if item['nome'] == nome:
+            item['quantidade'] += quantidade
+            return f"{quantidade} unidades de '{nome}' foram adicionadas ao estoque."
+    estoque_insumos.append({'nome': nome, 'quantidade': quantidade})
+    return f"{quantidade} unidades de '{nome}' foram adicionadas ao estoque."
 
-    def abrir_estoque():
-        acessar_estoque_gui()
+def remover_insumo(nome):
+    global estoque_insumos
+    for item in estoque_insumos:
+        if item['nome'] == nome:
+            estoque_insumos.remove(item)
+            return f"{nome} foi removido do estoque."
+    return f"{nome} não encontrado no estoque."
 
-    frame = tk.Frame(root)
-    frame.pack(pady=20)
+def obter_insumos():
+    return estoque_insumos
 
-    btn_caixa = tk.Button(frame, text="Acessar Caixa", width=30, height=3, command=abrir_caixa)
-    btn_caixa.grid(row=0, column=0, padx=10, pady=5)
+def tela_principal(root):
+    for widget in root.winfo_children():
+        widget.destroy()
 
-    btn_estoque = tk.Button(frame, text="Acessar Estoque", width=30, height=3, command=abrir_estoque)
-    btn_estoque.grid(row=1, column=0, padx=10, pady=5)
+    root.geometry("1280x720")
+    tk.Label(root, text="Sistema de Gerenciamento", font=("Helvetica", 24)).pack(pady=30)
+    tk.Button(root, text="Acessar Caixa", command=lambda: acessar_caixa(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Acessar Estoque", command=lambda: acessar_estoque(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Sair", command=root.quit, width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
 
-    btn_sair = tk.Button(frame, text="Sair", width=30, height=3, command=root.destroy)
-    btn_sair.grid(row=2, column=0, padx=10, pady=5)
+def gerenciar_ingredientes_tela(root):
+    for widget in root.winfo_children():
+        widget.destroy()
 
-    root.mainloop()
+    root.geometry("1280x720")
+    tk.Label(root, text="Gerenciar Ingredientes", font=("Helvetica", 24)).pack(pady=30)
+    tk.Button(root, text="Adicionar Insumo", command=lambda: adicionar_insumo_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Remover Insumo", command=lambda: remover_insumo_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Ver Insumos", command=lambda: ver_insumos_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Voltar ao Menu Principal", command=lambda: tela_principal(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+
+def adicionar_insumo_tela(root):
+    def confirmar_adicao():
+        nome_insumo = entry_nome_insumo.get().upper()
+        quantidade = entry_quantidade.get()
+
+        if not quantidade.isdigit() or int(quantidade) <= 0:
+            messagebox.showerror("Erro", "Por favor insira somente números inteiros e positivos para a quantidade.")
+            return
+
+        quantidade = int(quantidade)
+        mensagem = adicionar_insumo(nome_insumo, quantidade)
+        messagebox.showinfo("Resultado", mensagem)
+        janela_adicao.destroy()
+
+    janela_adicao = tk.Toplevel(root)
+    janela_adicao.title("Adicionar Insumo")
+
+    tk.Label(janela_adicao, text="Nome do Insumo:").pack(pady=5)
+    entry_nome_insumo = tk.Entry(janela_adicao)
+    entry_nome_insumo.pack(pady=5)
+
+    tk.Label(janela_adicao, text="Quantidade:").pack(pady=5)
+    entry_quantidade = tk.Entry(janela_adicao)
+    entry_quantidade.pack(pady=5)
+
+    tk.Button(janela_adicao, text="Confirmar", command=confirmar_adicao).pack(pady=20)
+
+def remover_insumo_tela(root):
+    def confirmar_remocao():
+        nome_insumo = entry_nome_insumo.get().upper()
+        mensagem = remover_insumo(nome_insumo)
+        messagebox.showinfo("Resultado", mensagem)
+        janela_remocao.destroy()
+
+    janela_remocao = tk.Toplevel(root)
+    janela_remocao.title("Remover Insumo")
+
+    tk.Label(janela_remocao, text="Nome do Insumo:").pack(pady=5)
+    entry_nome_insumo = tk.Entry(janela_remocao)
+    entry_nome_insumo.pack(pady=5)
+
+    tk.Button(janela_remocao, text="Confirmar", command=confirmar_remocao).pack(pady=20)
+
+def ver_insumos_tela(root):
+    insumos = obter_insumos()
+
+    janela_insumos = tk.Toplevel(root)
+    janela_insumos.title("Ver Insumos")
+
+    if not insumos:
+        tk.Label(janela_insumos, text="Estoque de insumos vazio.").pack(pady=20)
+    else:
+        tk.Label(janela_insumos, text="INSUMO\t\tQUANTIDADE").pack(pady=5)
+        for item in insumos:
+            insumo_info = f"{item['nome']}\t\t{item['quantidade']}"
+            tk.Label(janela_insumos, text=insumo_info).pack()
+
+def acessar_caixa(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    root.geometry("1280x720")
+    tk.Label(root, text="Caixa", font=("Helvetica", 24)).pack(pady=30)
+    tk.Button(root, text="Registrar Venda", command=lambda: registrar_venda_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Ver Histórico de Vendas", command=lambda: ver_historico_vendas_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Calcular Total Vendido", command=lambda: calcular_total_vendido_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Voltar ao Menu Principal", command=lambda: tela_principal(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+
+def registrar_venda_tela(root):
+    def confirmar_venda():
+        nome_produto = entry_nome_produto.get().upper()
+        quantidade = entry_quantidade.get()
+        preco = entry_preco.get()
+
+        if not quantidade.isdigit() or int(quantidade) <= 0:
+            messagebox.showerror("Erro", "Por favor insira somente números inteiros e positivos para a quantidade.")
+            return
+
+        if not preco.replace('.', '', 1).isdigit() or float(preco) <= 0:
+            messagebox.showerror("Erro", "Por favor insira um valor positivo para o preço.")
+            return
+
+        quantidade = int(quantidade)
+        preco = float(preco)
+
+        mensagem = registrar_venda(nome_produto, quantidade, preco)
+        messagebox.showinfo("Resultado", mensagem)
+        janela_registro.destroy()
+
+    janela_registro = tk.Toplevel(root)
+    janela_registro.title("Registrar Venda")
+
+    tk.Label(janela_registro, text="Nome do Produto:").pack(pady=5)
+    entry_nome_produto = tk.Entry(janela_registro)
+    entry_nome_produto.pack(pady=5)
+
+    tk.Label(janela_registro, text="Quantidade:").pack(pady=5)
+    entry_quantidade = tk.Entry(janela_registro)
+    entry_quantidade.pack(pady=5)
+
+    tk.Label(janela_registro, text="Preço Unitário:").pack(pady=5)
+    entry_preco = tk.Entry(janela_registro)
+    entry_preco.pack(pady=5)
+
+    tk.Button(janela_registro, text="Confirmar", command=confirmar_venda).pack(pady=20)
+
+def ver_historico_vendas_tela(root):
+    vendas = obter_historico_vendas()
+
+    janela_historico = tk.Toplevel(root)
+    janela_historico.title("Histórico de Vendas")
+
+    if not vendas:
+        tk.Label(janela_historico, text="Nenhuma venda registrada.").pack(pady=20)
+    else:
+        tk.Label(janela_historico, text="DATA\t\t\tPRODUTO\t\tQTD\tPREÇO UNITÁRIO").pack(pady=5)
+        for venda in vendas:
+            venda_info = f"{venda['data']}\t{venda['produto']}\t\t{venda['qtd']}\t{venda['preco']}"
+            tk.Label(janela_historico, text=venda_info).pack()
+
+def calcular_total_vendido_tela(root):
+    total = calcular_total_vendido()
+    messagebox.showinfo("Total Vendido", f"O total vendido é: R${total:.2f}")
+
+def acessar_estoque(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    root.geometry("1280x720")
+    tk.Label(root, text="Estoque", font=("Helvetica", 24)).pack(pady=30)
+    tk.Button(root, text="Adicionar Produto", command=lambda: adicionar_produto_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Remover Produto", command=lambda: remover_produto_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Ver Estoque", command=lambda: ver_estoque_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Gerenciamento de Insumos", command=lambda: gerenciar_ingredientes_tela(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+    tk.Button(root, text="Voltar ao Menu Principal", command=lambda: tela_principal(root), width=30, height=2, font=("Helvetica", 14)).pack(pady=10)
+
+
+def adicionar_produto_tela(root):
+    def confirmar_adicao():
+        nome_produto = entry_nome_produto.get().upper()
+        preco = entry_preco.get()
+
+        if not preco.replace('.', '', 1).isdigit() or float(preco) <= 0:
+            messagebox.showerror("Erro", "Por favor insira um preço válido.")
+            return
+
+        preco = float(preco)
+        mensagem = adicionar_produto(nome_produto, preco)
+        messagebox.showinfo("Resultado", mensagem)
+        janela_adicao.destroy()
+
+    janela_adicao = tk.Toplevel(root)
+    janela_adicao.title("Adicionar Produto")
+
+    tk.Label(janela_adicao, text="Nome do Produto:").pack(pady=5)
+    entry_nome_produto = tk.Entry(janela_adicao)
+    entry_nome_produto.pack(pady=5)
+
+    tk.Label(janela_adicao, text="Preço:").pack(pady=5)
+    entry_preco = tk.Entry(janela_adicao)
+    entry_preco.pack(pady=5)
+
+    tk.Button(janela_adicao, text="Confirmar", command=confirmar_adicao).pack(pady=20)
+
+def remover_produto_tela(root):
+    def confirmar_remocao():
+        nome_produto = entry_nome_produto.get().upper()
+        mensagem = remover_produto(nome_produto)
+        messagebox.showinfo("Resultado", mensagem)
+        janela_remocao.destroy()
+
+    janela_remocao = tk.Toplevel(root)
+    janela_remocao.title("Remover Produto")
+
+    tk.Label(janela_remocao, text="Nome do Produto:").pack(pady=5)
+    entry_nome_produto = tk.Entry(janela_remocao)
+    entry_nome_produto.pack(pady=5)
+
+    tk.Button(janela_remocao, text="Confirmar", command=confirmar_remocao).pack(pady=20)
+
+def ver_estoque_tela(root):
+    estoque = obter_estoque()
+
+    janela_estoque = tk.Toplevel(root)
+    janela_estoque.title("Ver Estoque")
+
+    if not estoque:
+        tk.Label(janela_estoque, text="Estoque vazio.").pack(pady=20)
+    else:
+        tk.Label(janela_estoque, text="PRODUTO\t\tPREÇO").pack(pady=5)
+        for item in estoque:
+            produto_info = f"{item['nome']}\t\t{item['preco']}"
+            tk.Label(janela_estoque, text=produto_info).pack()
 
 if __name__ == "__main__":
-    main_gui()
+    root = tk.Tk()
+    tela_principal(root)
+    root.mainloop()
