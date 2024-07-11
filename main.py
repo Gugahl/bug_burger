fa2d179a1af571b2fa031a9d92998871caea1c92
Gustavo Lima <ustavo.santos@gmail.com>
2024-07-11 12:57:50 -0300

    Documentação

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ altura = 720
 largura = 1280
 historico_movimentacoes = []
 
-# Define functions for handling stock and sales, similar to the previously provided code
-
 class Application:
+    # Inicialização de variáveis
     def __init__(self, root):
         self.root = root
         self.tela()
@@ -17,38 +16,45 @@ class Application:
         self.frame_estoque = Frame(root)
         self.mostrar_menu_principal()
 
+    # Criação da janela
     def tela(self):
         self.root.title("Gerenciamento de Restaurante")
         self.root.configure(background='#2E2E2E')
         self.root.geometry(f"{largura}x{altura}")
         self.root.resizable(True, True)
 
+    # Criação da tela do menu principal
     def mostrar_menu_principal(self):
         self.frame_main = Frame(self.root, bd=8, bg='#D5D5D5', highlightbackground='black', highlightthickness=2)
         self.frame_main.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
 
+        # Título da tela
         Label(self.frame_main, text="BugBurger", font=('Helvetica', 36, 'bold'), bg='#D5D5D5').place(relx=0.35, rely=0.05, relwidth=0.3, relheight=0.1)
-        
+
         # Estilizando os botões
         style = ttk.Style()
         style.configure('TButton', font=('Helvetica', 18), padding=10)
         style.map('TButton', background=[('active', '#BEBEBE')])
 
+        # Criação dos botões
         ttk.Button(self.frame_main, text="Acessar caixa", command=self.mostrar_caixa).place(relx=0.35, rely=0.25, relwidth=0.3, relheight=0.1)
         ttk.Button(self.frame_main, text="Acessar estoque", command=self.mostrar_estoque).place(relx=0.35, rely=0.4, relwidth=0.3, relheight=0.1)
         ttk.Button(self.frame_main, text="Sair", command=self.root.destroy).place(relx=0.35, rely=0.55, relwidth=0.3, relheight=0.1)
 
+    # Criação da tela do estoque
     def mostrar_estoque(self):
         self.frame_main.place_forget()
         self.frame_estoque = Frame(self.root, bd=8, bg='#E8E8E8', highlightbackground='#363636', highlightthickness=3)
         self.frame_estoque.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
-        
+
+        # Título da tela
         Label(self.frame_estoque, text="Estoque", font=('Arial', 30), bg='#E8E8E8', fg='#363636').place(relx=0.35, rely=0.05, relwidth=0.3, relheight=0.1)
-        
+
+        # Criação dos botões
         self.bt_adicionar = Button(self.frame_estoque, text="Adicionar produto", command=lambda: form_adicionar_produto(self), bg='#363636', fg='white')
         self.bt_adicionar.place(relx=0.35, rely=0.20, relwidth=0.3, relheight=0.1)
         self.bt_adicionar.config(font=('Arial', 12))
-        
+
         self.bt_remover = Button(self.frame_estoque, text='Remover produto', command=lambda: form_remover_produto(self), bg='#363636', fg='white')
         self.bt_remover.place(relx=0.35, rely=0.35, relwidth=0.3, relheight=0.1)
         self.bt_remover.config(font=('Arial', 12))
@@ -61,16 +67,19 @@ class Application:
         self.bt_sair.place(relx=0.35, rely=0.65, relwidth=0.3, relheight=0.1)
         self.bt_sair.config(font=('Arial', 12))
 
+    # Criação da tela do caixa
     def mostrar_caixa(self):
+        # Inicialização das variáveis
         self.frame_main.place_forget()
         nome_arquivo_vendas = 'historico.csv'
         vendas = ler_vendas(nome_arquivo_vendas)
-        
         self.frame_caixa = Frame(self.root, bd=8, bg='#E8E8E8', highlightbackground='#363636', highlightthickness=3)
         self.frame_caixa.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
 
+        # Título da tela
         Label(self.frame_caixa, text="Caixa", font=('Arial Black', 30), bg='#E8E8E8').place(relx=0.35, rely=0.05, relwidth=0.3, relheight=0.1)
 
+        # Criação dos botões
         self.bt_caixa = Button(self.frame_caixa, text="Registrar Venda", command=lambda: form_registrar_venda(self, nome_arquivo_vendas), bg='#363636', fg='white')
         self.bt_caixa.place(relx=0.35, rely=0.20, relwidth=0.3, relheight=0.1)
 
@@ -83,14 +92,17 @@ class Application:
         self.bt_sair = Button(self.frame_caixa, text='Voltar ao Menu Principal', command=lambda: self.voltar_menu_principal(self.frame_caixa), bg='#363636', fg='white')
         self.bt_sair.place(relx=0.35, rely=0.65, relwidth=0.3, relheight=0.1)
 
+    # Função de voltar para o menu principal
     def voltar_menu_principal(self, frame_atual):
         frame_atual.place_forget()
         self.mostrar_menu_principal()
 
+# Função para a chamada da aplicação
 def main():
     root = Tk()
     Application(root)
     root.mainloop()
 
+# Criado para termos certeza que o programa vai ser executado primeiro
 if __name__ == "__main__":
     main()
